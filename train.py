84fb9277b470db4531af6cf9cde05c2bfe97f2df
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-06-03 11:41:23 +0200

    AddedAllDataAugmantation

--- train.py
+++ train.py
@@ -18,6 +18,9 @@ def get_transform():
     train_transform = Compose([
         RandomResizedCrop(320, (0.5, 2.0)),
         RandomHorizontalFlip(),
+        RandomVerticalFlip(),
+        RandomRotation(180),
+        ColorJitter(0.5, 0.5, 0.5, 0.3),
         ToTensor(),
         Normalize(mean=[0.485, 0.456, 0.406],
                   std=[0.229, 0.224, 0.225]),
