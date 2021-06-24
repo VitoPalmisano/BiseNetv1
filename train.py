09349b4c691a3243f55df781d62f467b3ea4930b
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-06-24 18:04:34 +0200

    Commentato la classe train

--- train.py
+++ train.py
@@ -38,10 +38,21 @@ def get_transform():
 def val(args, model, dataloader):
     print('start val!')
     # label_info = get_label_info(csv_path)
+    '''
+    model.eval() is a kind of switch for some specific layers/parts of the model that
+    behave differently during training and inference (evaluating) time. For example, 
+    Dropouts Layers, BatchNorm Layers etc. You need to turn off them during model evaluation, 
+    and .eval() will do it for you. In addition, the common practice for evaluating/validation 
+    is using torch.no_grad() in pair with model.eval() to turn off gradients computation.
+    After that we call model.train() to reswitch them on.
+    '''
     with torch.no_grad():
         model.eval()
+        # lista delle precisioni
         precision_record = []
         hist = np.zeros((args.num_classes, args.num_classes))
+        
+        
         for i, (data, label) in enumerate(dataloader):
             if torch.cuda.is_available() and args.use_gpu:
                 data = data.cuda()
@@ -78,41 +89,76 @@ def val(args, model, dataloader):
 
 
 def train(args, model, optimizer, dataloader_train, dataloader_val):
+    # E' l'oggetto che ci stampa a schermo ciò chee acca
     writer = SummaryWriter(comment=''.format(args.optimizer, args.context_path))
+    # settiamo la loss
     if args.loss == 'dice':
+        # classe definita da loro nel file loss.py
         loss_func = DiceLoss()
     elif args.loss == 'crossentropy':
         loss_func = torch.nn.CrossEntropyLoss(ignore_index=255)
+    # inizializziamo i contatori
     max_miou = 0
     step = 0
+    # iniziamo il training
     for epoch in range(args.num_epochs):
+        # inizializziamo il learning rate
         lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
+        # iniziamo il train
         model.train()
+        # cosa grafica sequenziale
         tq = tqdm(total=len(dataloader_train) * args.batch_size)
         tq.set_description('epoch %d, lr %f' % (epoch, lr))
+        # Crediamo che sia la lista delle loss di ogni batch:
         loss_record = []
+        
+        # per ogni immagine o per ogni batch??? Ipotizziamo sia su ogni singolo mini-batch
         for i, (data, label) in enumerate(dataloader_train):
+            
             if torch.cuda.is_available() and args.use_gpu:
                 data = data.cuda()
                 label = label.cuda().long()
 
+            # Prendiamo: 
+            # - risultato finale dopo FFM
+            # - risultato del 16xdown del contextPath, dopo ARM, modificati (?)
+            # - risultato del 32xdown del contextPath, dopo ARM, modificati (?)
             output, output_sup1, output_sup2 = model(data)
+            
+            # Calcoliammo la loss 
+            # Principal loss function (l_p in the paper):
             loss1 = loss_func(output, label)
+            # Auxilary loss functions (l_i, for i=2, 3 in the paper):
             loss2 = loss_func(output_sup1, label)
             loss3 = loss_func(output_sup2, label)
+            
+            # alfa = 1, compute equation 2:
             loss = loss1 + loss2 + loss3
+            
+            # codice grafica
             tq.update(args.batch_size)
             tq.set_postfix(loss='%.6f' % loss)
+            
+            '''
+            zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
+            loss.backward() computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
+            opt.step() causes the optimizer to take a step based on the gradients of the parameters.
+            '''
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            
+            # incrementiamo il contatore
             step += 1
+            # aggiungiamo i valori per il grafico
             writer.add_scalar('loss_step', loss, step)
             loss_record.append(loss.item())
         tq.close()
         loss_train_mean = np.mean(loss_record)
         writer.add_scalar('epoch/loss_epoch_train', float(loss_train_mean), epoch)
         print('loss for train : %f' % (loss_train_mean))
+        
+        # salva il modello fin ora trainato
         if epoch % args.checkpoint_step == 0 and epoch != 0:
             import os
             if not os.path.isdir(args.save_model_path):
@@ -120,16 +166,24 @@ def train(args, model, optimizer, dataloader_train, dataloader_val):
             torch.save(model.state_dict(),
                        os.path.join(args.save_model_path, 'model.pth'))
 
+        # compute validation every 10 epochs
         if epoch % args.validation_step == 0 and epoch != 0:
+            
+            # chaiam la funzione val che da in output le metriche
             precision, miou = val(args, model, dataloader_val)
+            
+            # salva miou max e salva il relativo miglior modello
             if miou > max_miou:
                 max_miou = miou
                 import os
                 os.makedirs(args.save_model_path, exist_ok=True)
                 torch.save(model.state_dict(),
                            os.path.join(args.save_model_path, 'best_dice_loss.pth'))
+                
             writer.add_scalar('epoch/precision_val', precision, epoch)
             writer.add_scalar('epoch/miou val', miou, epoch)
+    # proviamo a terminare il writer per vedere se stampa qualcosa
+    writer.close()
 
 
 def main(params):
@@ -153,12 +207,14 @@ def main(params):
     parser.add_argument('--optimizer', type=str, default='rmsprop', help='optimizer, support rmsprop, sgd, adam')
     parser.add_argument('--loss', type=str, default='crossentropy', help='loss function, dice or crossentropy')
 
+    # settiamo i nostri parametri
     args = parser.parse_args(params)
 
     # create dataset and dataloader
     train_path = args.data
     train_transform, val_transform = get_transform()
-
+    
+    # creiamo un oggetto di tipo VOC per il training
     dataset_train = VOC(train_path, image_set="train", transform=train_transform)
     dataloader_train = DataLoader(
         dataset_train,
@@ -167,6 +223,8 @@ def main(params):
         num_workers=args.num_workers,
         drop_last=True
     )
+    
+    # creiamo un oggetto di tipo VOC per la validation
     dataset_val = VOC(train_path, image_set="val", transform=val_transform)
     dataloader_val = DataLoader(
         dataset_val,
@@ -193,18 +251,21 @@ def main(params):
         return None
 
     # load pretrained model if exists
+    # Non ce l'abbiamo
     if args.pretrained_model_path is not None:
         print('load model from %s ...' % args.pretrained_model_path)
         model.load_state_dict(torch.load(args.pretrained_model_path))
         print('Done!')
 
     # train
+    # funzioni presenti in questo file
     train(args, model, optimizer, dataloader_train, dataloader_val)
 
     val(args, model, dataloader_val)
 
 
 if __name__ == '__main__':
+    # settiamo i parametri che vogliamo cambiare
     params = [
         '--num_epochs', '30',
         '--learning_rate', '1e-3',
